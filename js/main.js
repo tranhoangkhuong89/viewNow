00037b453f7998296eb5d60f9ad0bfca35dd9b21
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-08 10:24:46 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -119,7 +119,7 @@ if (loadUrlDB == null) {
 	var dt=now.toLocaleDateString('en-GB').split('/').join('-');
 	var dbx = new Dropbox.Dropbox({ accessToken: 'jNfuqaYoI3AAAAAAAAAAqvr96aupCnGYWhhPaL2m6A0r6UxWV4nBF8XwARehWV25', fetch: fetch });
 	var ur='/Dropbox/DotNetApi/'+dt+'_resDB_test.db';
-	dbx.filesDownload({path: ur;})
+	dbx.filesDownload({path: ur})
 		.then(function(response) {
 			var reader = new FileReader();
 			reader.onload = function(event) {
