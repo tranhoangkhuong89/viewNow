743473d822e85e0c17f0f760189aabfdb2f6f808
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-08 16:18:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -522,3 +522,123 @@ function renderQuery(query) {
         positionFooter();
     }, 100);
 }
+
+function executeTK() {
+    var query = "SELECT [table], count(*)  as sl, sum(price) as total FROM 'order' WHERE [table] is not null GROUP BY [table]";
+    renderQuery(query);
+    $("#tables").select2("val", getTableNameFromQuery(query));
+}
+function renderQueryTK(query) {
+    var dataBox = $("#data");
+    var thead = dataBox.find("thead").find("tr");
+    var tbody = dataBox.find("tbody");
+
+    thead.empty();
+    tbody.empty();
+    errorBox.hide();
+    dataBox.show();
+
+    var columnTypes = [];
+    var tableName = getTableNameFromQuery(query);
+    if (tableName != null) {
+        columnTypes = getTableColumnTypes(tableName);
+    }
+
+    var sel;
+    try {
+        sel = db.prepare(query);
+    } catch (ex) {
+        showError(ex);
+        return;
+    }
+
+    var addedColums = false;
+   var stt=1;
+   var total=0;
+    while (sel.step()) {
+        /* if (!addedColums) {
+            addedColums = true;
+		if(tableName=="order"){
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">stt</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">table</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">price</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">sTime</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">eTime</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">checkout</span></th>');
+		   }
+		   else{
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">stt</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">food&drink</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">price</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">sl</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">total</span></th>');
+		   }
+            /*var columnNames = sel.getColumnNames();
+            for (var i = 0; i < columnNames.length; i++) {
+                var type = columnTypes[columnNames[i]];
+                thead.append('<th><span data-toggle="tooltip" data-placement="top" title="' + type + '">' + columnNames[i] + "</span></th>");
+            }*/
+        } */
+
+        var tr = $('<tr>');
+        var s = sel.get();
+        for (var i = 0; i < s.length; i++) {
+           if(i==0){
+               tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + stt + '</span></td>');
+           }
+           else if(tableName=="order"){
+              if(i==2){
+                 total+=s[i];
+                 var t=s[i];
+                 tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(Number(t).toLocaleString()) + '</span></td>');
+              }
+              else{
+                 if(i==3 || i==4){
+                     var bb="";
+                     if(s[i]!=null){
+                        bb=s[i].toString().split(' ')[1];
+                     }
+                     tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(bb) + '</span></td>');
+                 }
+                 else if(i==5){
+                  if(s[i]=="1"){
+                     tr.append('<td><span style="background-color:green" title="' + htmlEncode(s[i]) + '">_____</span></td>');
+                  }
+                    else{
+                     tr.append('<td><span style="background-color:red" title="' + htmlEncode(s[i]) + '">_____</span></td>');
+                    }
+                 }
+                 else
+                  tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+              }
+           }
+            else{
+               if(i==2){
+                  tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + Number(s[i]).toLocaleString() + '</span></td>');
+               }
+		else if(i==4){
+			var price=s[2];
+			var sl=s[3]
+			var tt=price*sl;
+			total+=tt;
+                  tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + Number(tt).toLocaleString() + '</span></td>');
+               }    
+               else{
+                  tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+               }
+               
+            }
+        }
+        tbody.append(tr);
+       stt++;
+    }
+	$('#total').text("Total: "+Number(total).toLocaleString());
+    refreshPagination(query, tableName);
+
+    $('[data-toggle="tooltip"]').tooltip({html: true});
+    dataBox.editableTableWidget();
+
+    setTimeout(function () {
+        positionFooter();
+    }, 100);
+}
