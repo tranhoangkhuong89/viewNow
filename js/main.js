bf7142b2b45e8a71b12a2d49913fa40bd529ebe3
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2018-08-08 10:22:45 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,8 @@ if (loadUrlDB == null) {
 	var now = new Date();
 	var dt=now.toLocaleDateString('en-GB').split('/').join('-');
 	var dbx = new Dropbox.Dropbox({ accessToken: 'jNfuqaYoI3AAAAAAAAAAqvr96aupCnGYWhhPaL2m6A0r6UxWV4nBF8XwARehWV25', fetch: fetch });
-	dbx.filesDownload({path: '/Dropbox/DotNetApi/'+dt+'_resDB_test.db'})
+	var ur='/Dropbox/DotNetApi/'+dt+'_resDB_test.db';
+	dbx.filesDownload({path: ur;})
 		.then(function(response) {
 			var reader = new FileReader();
 			reader.onload = function(event) {
