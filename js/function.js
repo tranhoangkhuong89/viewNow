9d2e3b00f5f46d3314adb1eb5934922d9b474e39
tranhoangkhuong89 <tranhoangkhuong89@gmail.com>
2019-01-17 16:40:44 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -70,6 +70,9 @@ function executeMon() {
 
     renderQueryBo();
     renderQueryMi()
+    renderQueryPizza();
+    renderQueryKhoang();
+
 }
 
 function renderQueryBo() {
@@ -202,6 +205,146 @@ function renderQueryMi() {
        stt++;
     }
 	$('#idmi').text("Mì: "+total_mi.toString()+" phần");
+    //refreshPagination(query, tableName);
+
+    $('[data-toggle="tooltip"]').tooltip({html: true});
+    dataBox.editableTableWidget();
+
+    setTimeout(function () {
+        positionFooter();
+    }, 100);
+}
+
+function renderQueryPizza() {
+  var query = "select i.id, i.name,i.sl,o.[table],o.price,o.starttime,o.endtime,o.checkout from item i inner join 'order' o on i.idorder=o.idkey where i.name like 'Pizza%' order by o.starttime asc";
+    var dataBox = $("#tblpizza");
+    var thead = dataBox.find("thead").find("tr");
+    var tbody = dataBox.find("tbody");
+
+    thead.empty();
+    tbody.empty();
+    errorBox.hide();
+    dataBox.show();
+
+
+    var sel;
+    try {
+        sel = db.prepare(query);
+    } catch (ex) {
+        showError(ex);
+        return;
+    }
+    var addedColums = false;
+   var stt=1;
+   var total=0;
+    while (sel.step()) {
+        if (!addedColums) {
+            addedColums = true;
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">stt</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">food</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">sl</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">table</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">price</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">in</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">out</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">checked</span></th>');
+        }
+        var tr = $('<tr>');
+
+        var s = sel.get();
+        for (var i = 0; i < s.length; i++) {
+           if(i==0){
+               tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + stt + '</span></td>');
+           }
+           else{
+              if(i==4){
+
+                 tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(Number(s[i]).toLocaleString()) + '</span></td>');
+              }
+              else{
+                    if(i==2){
+                    tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+                      total+=s[i];
+                      }
+                      else
+                      tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+                 }
+            }
+        }
+        tbody.append(tr);
+       stt++;
+    }
+	$('#idpizza').text("Pizza: "+total.toString()+" phần");
+    //refreshPagination(query, tableName);
+
+    $('[data-toggle="tooltip"]').tooltip({html: true});
+    dataBox.editableTableWidget();
+
+    setTimeout(function () {
+        positionFooter();
+    }, 100);
+}
+
+function renderQueryKhoang() {
+  var query = "select i.id, i.name,i.sl,o.[table],o.price,o.starttime,o.endtime,o.checkout from item i inner join 'order' o on i.idorder=o.idkey where i.name like 'Pizza%' order by o.starttime asc";
+    var dataBox = $("#tblkhoang");
+    var thead = dataBox.find("thead").find("tr");
+    var tbody = dataBox.find("tbody");
+
+    thead.empty();
+    tbody.empty();
+    errorBox.hide();
+    dataBox.show();
+
+
+    var sel;
+    try {
+        sel = db.prepare(query);
+    } catch (ex) {
+        showError(ex);
+        return;
+    }
+    var addedColums = false;
+   var stt=1;
+   var total=0;
+    while (sel.step()) {
+        if (!addedColums) {
+            addedColums = true;
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">stt</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">food</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">sl</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">table</span></th>');
+		   thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">price</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">in</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">out</span></th>');
+       thead.append('<th><span data-toggle="tooltip" data-placement="top" title="string">checked</span></th>');
+        }
+        var tr = $('<tr>');
+
+        var s = sel.get();
+        for (var i = 0; i < s.length; i++) {
+           if(i==0){
+               tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + stt + '</span></td>');
+           }
+           else{
+              if(i==4){
+
+                 tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(Number(s[i]).toLocaleString()) + '</span></td>');
+              }
+              else{
+                    if(i==2){
+                    tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+                      total+=s[i];
+                      }
+                      else
+                      tr.append('<td><span title="' + htmlEncode(s[i]) + '">' + htmlEncode(s[i]) + '</span></td>');
+                 }
+            }
+        }
+        tbody.append(tr);
+       stt++;
+    }
+	$('#idkhoang').text("Khoang: "+total.toString()+" phần");
     //refreshPagination(query, tableName);
 
     $('[data-toggle="tooltip"]').tooltip({html: true});
